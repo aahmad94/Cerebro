d4e8d0c1e444e6d913eb7e1ccce89d4113b0f225
aahmad94 <aka5vm@virginia.edu>
2023-06-21 20:07:55 -0400

    copy tweet URL via selenium actions API

---
+++ README.md
@@ -0,0 +1,22 @@
+# Friday Sailer
+
+## Use Selenium to scrape the last tweet URL
+
+Twitter API base tier is write only and we would be unable to use the API to get the last n tweets from the user. As such our workaround is to try to scrape the URL from the user's Twitter page. 
+
+The URL is not actually embedded in the HTML of the Twitter page, rather it's copied to the clipboard via an async request. As such we use Selenium to recreate the browser events to click through the user's Twitter page, specifically the *share button* of the last tweet and then the *copy URL item* from the menu:
+
+```python
+    share_btn = driver.find_element(By.CSS_SELECTOR, share_selector)
+    second_tweet_selector = driver.find_element(By.CSS_SELECTOR, second_tweet_selector)
+    action.scroll_to_element(second_tweet_selector)
+    action.pause(1)
+    action.click(share_btn)
+    action.pause(1)
+    action.click()
+    action.perform()
+    copied_text = pyperclip.paste()
+    print(copied_text)
+```
+
+<img src="fridaysailor_parse.gif" width="1024" height="665.6"/>
--- chrome.py
+++ chrome.py
@@ -3,6 +3,7 @@ import pyperclip
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
@@ -13,22 +14,29 @@ options.add_argument("--window-size=1920,1200")
 
 driver = webdriver.Chrome(options=options)
 driver.get("https://twitter.com/fridaysailer")
+action = ActionChains(driver)
 
-tweet_selector = "article[data-testid='tweet']"
-share_selector = tweet_selector + " " + "div[role='group'] div:nth-child(4)"
+tweet_selector = "[data-testid='cellInnerDiv']"
+second_tweet_selector = "[data-testid='cellInnerDiv']:nth-of-type(2)"
+share_selector = tweet_selector + " " + "div[role='group'] div:nth-child(5)"
 link_selector = "div[data-testid='Dropdown'] div"
+
+tweet_url = ""
+
 try:
     element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, tweet_selector))
+        EC.presence_of_element_located((By.CSS_SELECTOR, share_selector))
     )
 finally:
-    time.sleep(5)
-    driver.find_element(By.CSS_SELECTOR, share_selector).click()
-    # driver.find_element(By.CSS_SELECTOR, link_selector).click()
-    # copied_text = pyperclip.paste()
-    # print(copied_text)
-    
-    driver.save_screenshot('screenshot.png')
+    share_btn = driver.find_element(By.CSS_SELECTOR, share_selector)
+    second_tweet_selector = driver.find_element(By.CSS_SELECTOR, second_tweet_selector)
+    action.scroll_to_element(second_tweet_selector)
+    action.pause(1)
+    action.click(share_btn)
+    action.pause(1)
+    action.click()
+    action.perform()
+    tweet_url = pyperclip.paste()
     driver.quit()
 
     
