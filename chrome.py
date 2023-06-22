6b48a30c8e2a2f159e1166633482f8f625f26449
aahmad94 <aka5vm@virginia.edu>
2023-06-22 15:01:40 -0400

    convert script to class format

--- chrome.py
+++ chrome.py
@@ -9,35 +9,64 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
-options = Options()
-options.add_argument("--window-size=1920,1200")
+class ScrapeUserPage:
+    user = ""
+    base_url = "https://twitter.com/"
+    tweet_info = {}
+    tweet_selector = "[data-testid='cellInnerDiv']"
+    second_tweet_selector = tweet_selector + ":nth-of-type(2)"
+    share_selector = tweet_selector + " " + "div[role='group'] div:nth-child(5)"
+    link_selector = "div[data-testid='Dropdown'] div"
 
-driver = webdriver.Chrome(options=options)
-driver.get("https://twitter.com/fridaysailer")
-action = ActionChains(driver)
+    activeDriver = None
 
-tweet_selector = "[data-testid='cellInnerDiv']"
-second_tweet_selector = "[data-testid='cellInnerDiv']:nth-of-type(2)"
-share_selector = tweet_selector + " " + "div[role='group'] div:nth-child(5)"
-link_selector = "div[data-testid='Dropdown'] div"
+    def __init__(self, user="FridaySailer"):
+        self.user = user
+        self.activeDriver = self.driver()
 
-tweet_url = ""
 
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, share_selector))
-    )
-finally:
-    share_btn = driver.find_element(By.CSS_SELECTOR, share_selector)
-    second_tweet_selector = driver.find_element(By.CSS_SELECTOR, second_tweet_selector)
-    action.scroll_to_element(second_tweet_selector)
-    action.pause(1)
-    action.click(share_btn)
-    action.pause(1)
-    action.click()
-    action.perform()
-    tweet_url = pyperclip.paste()
-    driver.quit()
+    def driver(self):
+        options = Options()
+        options.add_argument("--window-size=1240,666")
 
-    
 
+        driver = webdriver.Chrome(options=options)
+        driver.get(self.base_url + self.user)
+        return driver
+
+    def awaitDriver(self):
+        element = WebDriverWait(self.activeDriver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, self.second_tweet_selector))
+        )
+
+    def getLastTweetURLAction(self):
+            action = ActionChains(self.activeDriver)
+            tweet = self.activeDriver.find_element(By.CSS_SELECTOR, self.tweet_selector)
+            second_tweet = self.activeDriver.find_element(By.CSS_SELECTOR, self.second_tweet_selector)
+            share_btn = self.activeDriver.find_element(By.CSS_SELECTOR, self.share_selector)
+
+            # need to scroll into view to click share btn
+            action.scroll_to_element(second_tweet)
+            action.pause(0.5)
+            action.click(share_btn)
+            action.pause(0.5)
+
+            action.click()
+            action.perform()
+            self.tweet_info["text"] = tweet.text
+            self.tweet_info["tweet_url"] = pyperclip.paste()
+            self.activeDriver.quit()
+            return self.tweet_info
+
+    def initAction(self, action):
+        try:
+            self.awaitDriver()
+        finally:
+            action()
+
+users = ["fridaysailer", "elonmusk", "realdonaldtrump"]
+for user in users:
+    scraper = ScrapeUserPage(user)
+    scraper.initAction(scraper.getLastTweetURLAction)
+    print(scraper.tweet_info["text"])
+    print("\n")
