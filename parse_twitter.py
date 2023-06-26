65a60329ec27289a8e43ce644e8d8df39ee2f0d6
aahmad94 <aka5vm@virginia.edu>
2023-06-25 23:37:01 -0400

    rm path arg for driver

--- parse_twitter.py
+++ parse_twitter.py
@@ -25,7 +25,6 @@ class ParseTwitter:
         options.add_argument("--window-size=1920,1200")
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         options.add_argument(f'user-agent={user_agent}')
-        options.add_argument(r"path='/usr/local/bin/chromedriver'")
 
         driver = webdriver.Chrome(options=options)
         driver.get(self.base_url + self.user)
