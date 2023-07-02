204527dbae635444e4f12c026d5e16f5191e6c35
aahmad94 <aka5vm@virginia.edu>
2023-07-02 11:45:30 -0700

    swap url for vxtwitter

--- parse_twitter.py
+++ parse_twitter.py
@@ -69,7 +69,7 @@ class ParseTwitter:
     
     def driver(self, url):
         options = Options()
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument("--no-sandbox")
         options.add_argument("--window-size=1920,1080")
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
