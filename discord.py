0c33eae066689f1c80ffc1f1556516cd6a15f959
aahmad94 <aka5vm@virginia.edu>
2023-07-10 01:13:20 -0700

    add openai api to summarize tweets

--- discord.py
+++ discord.py
@@ -1,9 +1,18 @@
+import os
+import openai
+
 from parse_twitter import ParseTwitter
 from datetime import datetime, timedelta
 from discord_webhook import DiscordWebhook
+from dotenv import load_dotenv
 
 class TwitterToDiscord:
+
     def __init__(self, webhook_url, users, tweets):
+        load_dotenv()
+        openai.organization = "org-zeOV533K5hdpS1CFEc4Ph8Mh"
+        openai.api_key = os.getenv("OPENAI_KEY")
+        
         self.tweets = tweets
         self.webhook_url = webhook_url
         self.users = users
@@ -20,9 +29,8 @@ class TwitterToDiscord:
 
             # format text content to send
             content = None
-            if tweet_url and tweet_date and tweet_text:
-                content = user + f" {tweet_date}" + \
-                    "\n" + tweet_text + "\n\n" + tweet_url + "\n"
+            if tweet_url and tweet_text:
+                content = f"** ChatGPT additional context:**\n"+ self.ask_gpt(tweet_text) + "\n\n" + tweet_url + "\n"
                 print(content)
                 print("-------------------------------------------------")
 
@@ -32,6 +40,12 @@ class TwitterToDiscord:
                 if len(self.tweets) > len(self.users):
                     self.fwd_tweet(user, tweet_date, content)
 
+    def ask_gpt(self, tweet_text):
+        prompt = "Provide context for the following Tweet:\n"
+        messages = [{"role": "user", "content": prompt + tweet_text}]
+        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+        reply = chat.choices[0].message.content
+        return reply 
                    
     def fwd_tweet(self, user, tweet_date, content):
         date = datetime.now()
