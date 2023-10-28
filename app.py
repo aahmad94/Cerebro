c12bfe99f173ac2ef75272d74874c6be2d7a1c79
aahmad94 <aka5vm@virginia.edu>
2023-10-28 14:15:48 -0400

    refactor app.py

--- app.py
+++ app.py
@@ -46,38 +46,54 @@ barrons_snap = {
     "info": "BARRON'S FRONT PAGE\n",
 }
 
+sent_hr = 0
+sent_minute = 0
+
 # configure app timezone
 def get_time():
     ny = timezone('America/New_York')
     return datetime.now(ny)
 
-sent_hr = 0
-while True:
-    now = get_time()
-    
-    if now.hour == 0 and now.minute < 10:
-        sent_hr = 0
 
-    # enter loop every hour starting at 8 AM
-    if now.hour % 8 <= 4 and now.hour > sent_hr % 24:
+def send_images(now, sent_hr):
+    # send every hour
+    if now.hour > sent_hr:
         # send until 12 pm and at end of day at 9 pm
         if now.hour % 8 <= 4 or now.hour == 21:  
             Screenshot(cerebro_webhook_url, market_watch_snap["url"], market_watch_snap["css"], market_watch_snap["modal"], market_watch_snap["info"]).snap()    
-        # send until 7 pm
+    
+    # send every 2 hours
+    if now.hour > sent_hr + 1:
+        # send until end of day at 9 pm
         if now.hour <= 19:
             Screenshot(cerebro_webhook_url, barrons_snap["url"], barrons_snap["css"], barrons_snap["modal"], barrons_snap["info"]).snap()
+
+
+def fwd_tweets(now, sent_minute):
+    # fwd every weekday betweent 7 am and 5 pm every minute, else fwd every 10 minutes
+    if now.weekday() <= 4 and now.hour >= 7 and now.hour <= 17 and now.minute > sent_minute + 1:
+        TwitterToDiscord(cerebro_webhook_url, cerebro_users, cerebro_dict)
+    elif now.weekday > 4 and now.minute % 10 == 0:
+        TwitterToDiscord(cerebro_webhook_url, cerebro_users, cerebro_dict)        
+
+    # friday post for fridaysailer webhook, between 8 am and 2 pm every 10 minutes
+    if now.weekday() == 4 and now.hour > 8 and now.hour < 14 and now.minute % 10 == 0:
+        TwitterToDiscord(fridaysailer_url, fridaysailer_users, fridaysailer_dict)
+    
+
+while True:
+    now = get_time()
+    if now.hour == 0 and now.minute < 10:
+        sent_hr = 0
+    if now.minute == 0:
+        sent_minute = 0
+
+    send_images(now, sent_hr)
+    fwd_tweets(now, sent_minute)
+
+    if now.hour > sent_hr:
         sent_hr = now.hour
+    if now.minute > sent_minute:
+        sent_minute = now.minute
 
-    # fwd every 2.5 minutes between 7am and 5pm EST every weekday
-    if now.weekday() <= 4 and now.hour >= 7 and now.hour <= 17:
-        # use discord webhook to send screenshot of econ calendar
-        TwitterToDiscord(cerebro_webhook_url, cerebro_users, cerebro_dict)
-        time.sleep(60)
 
-        # friday post for fridaysailer webhook
-        if now.weekday() == 4 and now.hour > 8 and now.hour < 14:
-            TwitterToDiscord(fridaysailer_url, fridaysailer_users, fridaysailer_dict)
-            time.sleep(2.5*60)
-    else:
-        TwitterToDiscord(cerebro_webhook_url, cerebro_users, cerebro_dict)
-        time.sleep(10*60)        
