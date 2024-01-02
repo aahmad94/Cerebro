3ae40f4f37871afc951f9935b1a4697420caeeb6
aahmad94 <aka5vm@virginia.edu>
2024-01-02 01:54:20 -0500

    specify path of chrome bin in prod env

--- parse_twitter.py
+++ parse_twitter.py
@@ -100,11 +100,15 @@ class ParseTwitter:
 
     def driver(self, url):
         options = Options()
+
         options.add_argument('--headless')
         options.add_argument("--no-sandbox")
         options.add_argument("--window-size=1920,1080")
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         options.add_argument(f'user-agent={user_agent}')
+
+        if os.getenv("ENV") == "prod":
+            options.binary_location = "/usr/bin/google-chrome"
         
         service = None
         service = Service(ChromeDriverManager().install())
